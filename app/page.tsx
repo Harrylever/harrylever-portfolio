af0f96b7340d84da6ddcdfc39d95e8bae87fecb2
Ukanah Dean <ukanah15thdean@gmail.com>
2025-06-19 12:09:06 +0100

    feat: cooking

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import { Hero } from "@/page-sections/app/hero";
-import { Projects } from "@/page-sections/app/projects";
-import { Skills } from "@/page-sections/app/skills";
-import { WorkExperience } from "@/page-sections/app/work-experience";
-import { PlayAudio } from "@/utils/play-audio";
+// import { Hero } from "@/page-sections/app/hero";
+// import { Projects } from "@/page-sections/app/projects";
+// import { Skills } from "@/page-sections/app/skills";
+// import { WorkExperience } from "@/page-sections/app/work-experience";
+// import { PlayAudio } from "@/utils/play-audio";
 
 export default function Home() {
   return (
-    <section className="relative flex flex-col items-center justify-center gap-4">
-      <PlayAudio />
+    <section className="relative min-h-screen flex flex-col items-center justify-center gap-4">
+      <p>Cooking 🧑‍🍳</p>
+      {/* <PlayAudio />
       <div className="fixed top-0 left-0 w-full h-screen bg-noise-start bg-contain bg-repeat opacity-30" />
       <div className="relative w-full max-w-3xl px-10 sm:px-12 lg:px-0 space-y-16 py-8 md:py-12 duration-300">
         <Hero />
         <WorkExperience />
         <Projects />
         <Skills />
-      </div>
+      </div> */}
     </section>
   );
 }
